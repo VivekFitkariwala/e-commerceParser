501d762999a630caf2551f22aabf5a2cf73a538c
Vivek Fitkariwala <vivekfitkariwala@gmail.com>
2020-01-26 10:46:45 +0530

    Adding mongouri

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,8 @@ export default {
 
   DB_DIALECT: process.env.DB_DIALECT || "mongo",
   MONGODB_URI:
-    process.env.MONGODB_URI || "mongodb://localhost:27017/example_db",
+    process.env.MONGODB_URI ||
+    "mongodb://heroku_08gdpgn3:jpipmitfhnumju7o1d45o15jeb@ds121089.mlab.com:21089/heroku_08gdpgn3",
   DB_NAME: process.env.DB_NAME || "example_db",
   DB_PASSWORD: process.env.DB_PASSWORD || "db-password",
   DB_PORT: process.env.DB_PORT || "27017",
